03a5c8d2a9ad01d1449d652f2fd6364bec099709
Simao Fonseca <simao.fonseca@bisect.pt>
2026-03-06 17:00:44 +0000

    mxl-sink: Write video and audio correctly with network streams
    Signed-off-by: Simao Fonseca <simao.fonseca@bisect.pt>
    (cherry picked from commit 20b243207d24c9f8358b401ffe664183f963ca55)

---
+++ rust/gst-mxl-rs/src/mxlsink/imp.rs
@@ -0,0 +1,429 @@
+// SPDX-FileCopyrightText: 2025 2025 Contributors to the Media eXchange Layer project.
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright (C) 2018 Sebastian Dröge <sebastian@centricular.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use gst::glib;
+use gst::prelude::*;
+use gst::subclass::prelude::*;
+use gst_base::prelude::BaseSinkExt;
+use gst_base::subclass::prelude::*;
+use gstreamer as gst;
+use gstreamer_audio as gst_audio;
+use gstreamer_base as gst_base;
+
+use mxl::MxlInstance;
+use mxl::config::get_mxl_so_path;
+use tracing::trace;
+
+use std::sync::LazyLock;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+
+use crate::mxlsink;
+use crate::mxlsink::state::Context;
+use crate::mxlsink::state::DEFAULT_DOMAIN;
+use crate::mxlsink::state::DEFAULT_FLOW_ID;
+use crate::mxlsink::state::Settings;
+use crate::mxlsink::state::State;
+use crate::mxlsink::state::init_state_with_audio;
+use crate::mxlsink::state::init_state_with_video;
+use crate::mxlsink::{render_audio, render_video};
+
+pub(crate) static CAT: LazyLock<gst::DebugCategory> = LazyLock::new(|| {
+    gst::DebugCategory::new("mxlsink", gst::DebugColorFlags::empty(), Some("MXL Sink"))
+});
+
+struct ClockWait {
+    clock_id: Option<gst::SingleShotClockId>,
+    flushing: bool,
+}
+
+impl Default for ClockWait {
+    fn default() -> ClockWait {
+        ClockWait {
+            clock_id: None,
+            flushing: true,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct MxlSink {
+    settings: Mutex<Settings>,
+    context: Mutex<Context>,
+    clock_wait: Mutex<ClockWait>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for MxlSink {
+    const NAME: &'static str = "GstRsMxlSink";
+    type Type = mxlsink::MxlSink;
+    type ParentType = gst_base::BaseSink;
+}
+
+impl ObjectImpl for MxlSink {
+    fn properties() -> &'static [glib::ParamSpec] {
+        static PROPERTIES: LazyLock<Vec<glib::ParamSpec>> = LazyLock::new(|| {
+            vec![
+                glib::ParamSpecString::builder("flow-id")
+                    .nick("FlowID")
+                    .blurb("Flow ID")
+                    .default_value(DEFAULT_FLOW_ID)
+                    .mutable_ready()
+                    .build(),
+                glib::ParamSpecString::builder("domain")
+                    .nick("Domain")
+                    .blurb("Domain")
+                    .default_value(DEFAULT_DOMAIN)
+                    .mutable_ready()
+                    .build(),
+            ]
+        });
+
+        PROPERTIES.as_ref()
+    }
+
+    fn constructed(&self) {
+        #[cfg(feature = "tracing")]
+        {
+            use tracing_subscriber::filter::LevelFilter;
+            use tracing_subscriber::util::SubscriberInitExt;
+            let _ = tracing_subscriber::fmt()
+                .compact()
+                .with_file(true)
+                .with_line_number(true)
+                .with_thread_ids(true)
+                .with_target(false)
+                .with_max_level(LevelFilter::TRACE)
+                .with_ansi(true)
+                .finish()
+                .try_init();
+        }
+        self.parent_constructed();
+        self.obj().set_sync(true);
+    }
+
+    fn set_property(&self, _id: usize, value: &glib::Value, pspec: &glib::ParamSpec) {
+        if let Ok(mut settings) = self.settings.lock() {
+            match pspec.name() {
+                "flow-id" => {
+                    if let Ok(flow_id) = value.get::<String>() {
+                        gst::info!(
+                            CAT,
+                            imp = self,
+                            "Changing flow-id from {} to {}",
+                            settings.flow_id,
+                            flow_id
+                        );
+                        settings.flow_id = flow_id;
+                    } else {
+                        gst::error!(CAT, imp = self, "Invalid type for flow-id property");
+                    }
+                }
+                "domain" => {
+                    if let Ok(domain) = value.get::<String>() {
+                        gst::info!(
+                            CAT,
+                            imp = self,
+                            "Changing domain from {} to {}",
+                            settings.domain,
+                            domain
+                        );
+                        settings.domain = domain;
+                    } else {
+                        gst::error!(CAT, imp = self, "Invalid type for domain property");
+                    }
+                }
+                other => {
+                    gst::error!(CAT, imp = self, "Unknown property '{}'", other);
+                }
+            }
+        } else {
+            gst::error!(
+                CAT,
+                imp = self,
+                "Settings mutex poisoned — property change ignored"
+            );
+        }
+    }
+
+    fn property(&self, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+        if let Ok(settings) = self.settings.lock() {
+            match pspec.name() {
+                "flow-id" => settings.flow_id.to_value(),
+                "domain" => settings.domain.to_value(),
+                _ => {
+                    gst::error!(CAT, imp = self, "Unknown property {}", pspec.name());
+                    glib::Value::from(&"")
+                }
+            }
+        } else {
+            gst::error!(CAT, imp = self, "Settings mutex poisoned");
+            glib::Value::from(&"")
+        }
+    }
+}
+
+impl GstObjectImpl for MxlSink {}
+
+impl ElementImpl for MxlSink {
+    fn metadata() -> Option<&'static gst::subclass::ElementMetadata> {
+        static ELEMENT_METADATA: LazyLock<gst::subclass::ElementMetadata> = LazyLock::new(|| {
+            gst::subclass::ElementMetadata::new(
+                "MXL Sink",
+                "Sink/Video",
+                "Generates an MXL flow from GStreamer buffers",
+                "Contributors to the Media eXchange Layer project",
+            )
+        });
+
+        Some(&*ELEMENT_METADATA)
+    }
+    fn pad_templates() -> &'static [gst::PadTemplate] {
+        static PAD_TEMPLATES: LazyLock<Result<Vec<gst::PadTemplate>, glib::BoolError>> =
+            LazyLock::new(|| {
+                let mut caps = gst::Caps::new_empty();
+                {
+                    let caps_mut = caps.make_mut();
+
+                    caps_mut.append(
+                        gst::Caps::builder("video/x-raw")
+                            .field("format", "v210")
+                            .build(),
+                    );
+                    for ch in 1..64 {
+                        let mask = gst::Bitmask::from((1u64 << ch) - 1);
+                        caps.make_mut().append(
+                            gst::Caps::builder("audio/x-raw")
+                                .field("format", "F32LE")
+                                .field("layout", "interleaved")
+                                .field("channels", ch)
+                                .field("channel-mask", mask)
+                                .build(),
+                        );
+                    }
+                }
+
+                let sink_pad_template = gst::PadTemplate::new(
+                    "sink",
+                    gst::PadDirection::Sink,
+                    gst::PadPresence::Always,
+                    &caps,
+                )?;
+
+                Ok(vec![sink_pad_template])
+            });
+
+        match PAD_TEMPLATES.as_ref() {
+            Ok(templates) => templates,
+            Err(err) => {
+                trace!("Failed to create pad templates: {:?}", err);
+                &[]
+            }
+        }
+    }
+    fn change_state(
+        &self,
+        transition: gst::StateChange,
+    ) -> Result<gst::StateChangeSuccess, gst::StateChangeError> {
+        self.parent_change_state(transition)
+    }
+}
+
+impl BaseSinkImpl for MxlSink {
+    fn start(&self) -> Result<(), gst::ErrorMessage> {
+        let mut context = self.context.lock().map_err(|e| {
+            gst::error_msg!(gst::CoreError::Failed, ["Failed to get state mutex: {}", e])
+        })?;
+        self.unlock_stop()?;
+        let settings = self.settings.lock().map_err(|e| {
+            gst::error_msg!(
+                gst::CoreError::Failed,
+                ["Failed to get settings mutex: {}", e]
+            )
+        })?;
+        let instance = init_mxl_instance(&settings)?;
+        let pipeline = self
+            .obj()
+            .parent()
+            .and_then(|p| p.downcast::<gst::Pipeline>().ok())
+            .ok_or(gst::error_msg!(
+                gst::CoreError::Failed,
+                ["Failed to get pipeline"]
+            ))?;
+        context.state = Some(State {
+            instance,
+            flow: None,
+            video: None,
+            audio: None,
+            pipeline,
+        });
+
+        Ok(())
+    }
+
+    fn stop(&self) -> Result<(), gst::ErrorMessage> {
+        let mut context = self.context.lock().map_err(|e| {
+            gst::error_msg!(
+                gst::CoreError::Failed,
+                ["Failed to get context mutex: {}", e]
+            )
+        })?;
+        self.unlock()?;
+        let state = context.state.as_mut().ok_or(gst::error_msg!(
+            gst::CoreError::Failed,
+            ["Failed to get state"]
+        ))?;
+
+        if let Some(audio) = state.audio.take() {
+            let (lock, cvar) = &*audio.sleep_flag;
+
+            let mut flag = lock.lock().map_err(|_| {
+                gst::error_msg!(gst::CoreError::Failed, ["Failed to get audio sleep lock"])
+            })?;
+            *flag = true;
+            cvar.notify_all();
+            drop(audio.tx);
+        }
+
+        if let Some(video) = state.video.take() {
+            let (lock, cvar) = &*video.sleep_flag;
+
+            let mut flag = lock.lock().map_err(|_| {
+                gst::error_msg!(gst::CoreError::Failed, ["Failed to get video sleep lock"])
+            })?;
+            *flag = true;
+            cvar.notify_all();
+            drop(video.tx);
+        }
+
+        gst::info!(CAT, imp = self, "Stopped");
+        Ok(())
+    }
+
+    fn render(&self, buffer: &gst::Buffer) -> Result<gst::FlowSuccess, gst::FlowError> {
+        trace!("START RENDER");
+
+        let mut context = self.context.lock().map_err(|_| gst::FlowError::Error)?;
+        let state = context.state.as_mut().ok_or(gst::FlowError::Error)?;
+        if state.video.is_some() {
+            render_video::video(state, buffer)
+        } else {
+            render_audio::audio(state, buffer)
+        }
+    }
+
+    fn prepare(&self, buffer: &gst::Buffer) -> Result<gst::FlowSuccess, gst::FlowError> {
+        self.parent_prepare(buffer)
+    }
+
+    fn render_list(&self, list: &gst::BufferList) -> Result<gst::FlowSuccess, gst::FlowError> {
+        self.parent_render_list(list)
+    }
+
+    fn prepare_list(&self, list: &gst::BufferList) -> Result<gst::FlowSuccess, gst::FlowError> {
+        self.parent_prepare_list(list)
+    }
+
+    fn query(&self, query: &mut gst::QueryRef) -> bool {
+        BaseSinkImplExt::parent_query(self, query)
+    }
+
+    fn event(&self, event: gst::Event) -> bool {
+        self.parent_event(event)
+    }
+
+    fn caps(&self, filter: Option<&gst::Caps>) -> Option<gst::Caps> {
+        self.parent_caps(filter)
+    }
+
+    fn set_caps(&self, caps: &gst::Caps) -> Result<(), gst::LoggableError> {
+        let mut context = self
+            .context
+            .lock()
+            .map_err(|e| gst::loggable_error!(CAT, "Failed to lock context mutex: {}", e))?;
+        let state = context
+            .state
+            .as_mut()
+            .ok_or(gst::loggable_error!(CAT, "Failed to get state",))?;
+
+        let settings = self
+            .settings
+            .lock()
+            .map_err(|e| gst::loggable_error!(CAT, "Failed to lock settings mutex: {}", e))?;
+
+        let structure = caps
+            .structure(0)
+            .ok_or_else(|| gst::loggable_error!(CAT, "No structure in caps {}", caps))?;
+        let name = structure.name();
+        if name == "audio/x-raw" {
+            let info = gst_audio::AudioInfo::from_caps(caps)
+                .map_err(|e| gst::loggable_error!(CAT, "Invalid audio caps: {}", e))?;
+
+            init_state_with_audio(state, info, &settings.flow_id)?;
+            Ok(())
+        } else {
+            init_state_with_video(state, structure, &settings.flow_id)
+        }
+    }
+
+    fn fixate(&self, caps: gst::Caps) -> gst::Caps {
+        self.parent_fixate(caps)
+    }
+
+    fn unlock(&self) -> Result<(), gst::ErrorMessage> {
+        gst::debug!(CAT, imp = self, "Unlocking");
+        let mut clock_wait = self.clock_wait.lock().map_err(|e| {
+            gst::error_msg!(gst::CoreError::Failed, ["Failed to lock clock: {}", e])
+        })?;
+        if let Some(clock_id) = clock_wait.clock_id.take() {
+            clock_id.unschedule();
+        }
+        clock_wait.flushing = true;
+
+        Ok(())
+    }
+
+    fn unlock_stop(&self) -> Result<(), gst::ErrorMessage> {
+        gst::debug!(CAT, imp = self, "Unlock stop");
+        let mut clock_wait = self.clock_wait.lock().map_err(|e| {
+            gst::error_msg!(gst::CoreError::Failed, ["Failed to lock clock: {}", e])
+        })?;
+        clock_wait.flushing = false;
+
+        Ok(())
+    }
+
+    fn propose_allocation(
+        &self,
+        query: &mut gst::query::Allocation,
+    ) -> Result<(), gst::LoggableError> {
+        self.parent_propose_allocation(query)
+    }
+}
+
+fn init_mxl_instance(
+    settings: &MutexGuard<'_, Settings>,
+) -> Result<MxlInstance, gst::ErrorMessage> {
+    let mxl_api = mxl::load_api(get_mxl_so_path())
+        .map_err(|e| gst::error_msg!(gst::CoreError::Failed, ["Failed to load MXL API: {}", e]))?;
+
+    let mxl_instance =
+        mxl::MxlInstance::new(mxl_api, settings.domain.as_str(), "").map_err(|e| {
+            gst::error_msg!(
+                gst::CoreError::Failed,
+                ["Failed to load MXL instance: {}", e]
+            )
+        })?;
+
+    Ok(mxl_instance)
+}
---
+++ rust/gst-mxl-rs/src/mxlsink/state.rs
@@ -0,0 +1,327 @@
+// SPDX-FileCopyrightText: 2025 2025 Contributors to the Media eXchange Layer project.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::{
+    collections::HashMap,
+    process,
+    str::FromStr,
+    sync::{Arc, Condvar, Mutex},
+    thread,
+};
+
+use crate::mxlsink::{
+    imp::CAT,
+    render_audio::{WriteGrainData, WriteSampleData, await_audio_buffer},
+    render_video::await_video_buffer,
+};
+use crossbeam::channel::bounded;
+use gst::StructureRef;
+use gst_audio::AudioInfo;
+use gstreamer as gst;
+use gstreamer_audio as gst_audio;
+use mxl::{
+    FlowConfigInfo, GrainWriter, MxlInstance, Rational, SamplesWriter,
+    flowdef::{
+        Component, FlowDef, FlowDefAudio, FlowDefDetails, FlowDefVideo, InterlaceMode, Rate,
+    },
+};
+use tracing::trace;
+
+use uuid::Uuid;
+
+pub(crate) const DEFAULT_FLOW_ID: &str = "";
+pub(crate) const DEFAULT_DOMAIN: &str = "";
+const MAX_CHANNEL_SIZE: usize = 50;
+
+#[derive(Debug, Clone)]
+pub(crate) struct Settings {
+    pub flow_id: String,
+    pub domain: String,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            flow_id: DEFAULT_FLOW_ID.to_owned(),
+            domain: DEFAULT_DOMAIN.to_owned(),
+        }
+    }
+}
+
+pub(crate) struct State {
+    pub instance: MxlInstance,
+    pub flow: Option<FlowConfigInfo>,
+    pub video: Option<VideoState>,
+    pub audio: Option<AudioState>,
+    pub pipeline: gst::Pipeline,
+}
+
+pub(crate) struct VideoState {
+    pub tx: crossbeam::channel::Sender<VideoCommand>,
+    pub grain_index: u64,
+    pub initial_time: Option<InitialTime>,
+    pub latency: u64,
+    pub sleep_flag: Arc<(Mutex<bool>, Condvar)>,
+}
+
+#[derive(Debug)]
+pub(crate) struct AudioState {
+    pub tx: crossbeam::channel::Sender<AudioCommand>,
+    pub flow_def: FlowDefAudio,
+    pub initial_time: Option<InitialTime>,
+    pub latency: u64,
+    pub sleep_flag: Arc<(Mutex<bool>, Condvar)>,
+}
+
+pub enum AudioCommand {
+    Write { data: WriteSampleData },
+}
+
+pub enum VideoCommand {
+    Write { data: WriteGrainData },
+}
+
+#[derive(Default)]
+pub(crate) struct Context {
+    pub state: Option<State>,
+}
+
+pub struct AudioEngine {
+    pub writer: Option<SamplesWriter>,
+    pub instance: MxlInstance,
+    pub sample_rate: Rational,
+    pub sleep_flag: Arc<(Mutex<bool>, Condvar)>,
+}
+
+pub struct VideoEngine {
+    pub writer: Option<GrainWriter>,
+    pub instance: MxlInstance,
+    pub grain_rate: Rational,
+    pub sleep_flag: Arc<(Mutex<bool>, Condvar)>,
+}
+
+#[derive(Debug, Clone)]
+pub struct InitialTime {
+    pub mxl_pts_offset: u64,
+}
+
+pub(crate) fn init_state_with_audio(
+    state: &mut State,
+    info: AudioInfo,
+    flow_id: &str,
+) -> Result<(), gst::LoggableError> {
+    let channels = info.channels() as i32;
+    let rate = info.rate() as i32;
+    let bit_depth = info.depth() as u8;
+    let format = info.format().to_string();
+    let pid = process::id();
+    let mut tags = HashMap::new();
+    tags.insert(
+        "urn:x-nmos:tag:grouphint/v1.0".to_string(),
+        vec![format!("Media Function {}:Audio", pid).to_string()],
+    );
+
+    let flow_def_details = FlowDefAudio {
+        sample_rate: Rate {
+            numerator: rate,
+            denominator: 1,
+        },
+        channel_count: channels,
+        bit_depth,
+    };
+
+    let flow_def = FlowDef {
+        id: Uuid::parse_str(flow_id)
+            .map_err(|e| gst::loggable_error!(CAT, "Flow ID is invalid: {}", e))?,
+        description: "MXL Audio Flow".into(),
+        format: "urn:x-nmos:format:audio".into(),
+        tags,
+        label: "MXL Audio Flow".into(),
+        media_type: "audio/float32".to_string(),
+        parents: vec![],
+        details: FlowDefDetails::Audio(flow_def_details.clone()),
+    };
+
+    let (flow_writer, flow, is_created) = state
+        .instance
+        .create_flow_writer(
+            serde_json::to_string(&flow_def)
+                .map_err(|e| gst::loggable_error!(CAT, "Failed to convert: {}", e))?
+                .as_str(),
+            None,
+        )
+        .map_err(|e| gst::loggable_error!(CAT, "Failed to create flow writer: {}", e))?;
+    let writer = Some(
+        flow_writer
+            .to_samples_writer()
+            .map_err(|e| gst::loggable_error!(CAT, "Failed to create grain writer: {}", e))?,
+    );
+    let (tx, rx) = bounded::<AudioCommand>(MAX_CHANNEL_SIZE);
+    let instance = state.instance.clone();
+    if !is_created {
+        return Err(gst::loggable_error!(
+            CAT,
+            "The writer could not be created, the UUID belongs to a flow with another active writer"
+        ));
+    }
+    let sleep_flag_init = Arc::new((Mutex::new(false), Condvar::new()));
+    let sleep_flag = sleep_flag_init.clone();
+    let audio_state = AudioState {
+        tx,
+        flow_def: flow_def_details,
+        initial_time: None,
+        latency: 0,
+        sleep_flag,
+    };
+    state.audio = Some(audio_state);
+    state.flow = Some(flow);
+
+    let sample_rate = Rational {
+        numerator: rate as i64,
+        denominator: 1,
+    };
+
+    let sleep_flag = sleep_flag_init.clone();
+    thread::spawn(move || {
+        let mut engine = AudioEngine {
+            writer,
+            instance,
+            sample_rate,
+            sleep_flag,
+        };
+        await_audio_buffer(&mut engine, rx)
+    });
+    trace!(
+        "Made it to the end of set_caps with format {}, channel_count {}, sample_rate {}, bit_depth {}",
+        format, channels, rate, bit_depth
+    );
+    Ok(())
+}
+
+pub(crate) fn init_state_with_video(
+    state: &mut State,
+    structure: &StructureRef,
+    flow_id: &str,
+) -> Result<(), gst::LoggableError> {
+    let format = structure
+        .get::<String>("format")
+        .unwrap_or_else(|_| "v210".to_string());
+    let width = structure.get::<i32>("width").unwrap_or(1920);
+    let height = structure.get::<i32>("height").unwrap_or(1080);
+    let framerate = structure
+        .get::<gst::Fraction>("framerate")
+        .unwrap_or_else(|_| gst::Fraction::new(30000, 1001));
+    let interlace = structure
+        .get::<String>("interlace-mode")
+        .unwrap_or_else(|_| "progressive".to_string());
+    let interlace_mode =
+        InterlaceMode::from_str(interlace.as_str()).unwrap_or(InterlaceMode::Progressive);
+    let colorimetry = structure
+        .get::<String>("colorimetry")
+        .unwrap_or_else(|_| "BT709".to_string());
+    let pid = process::id();
+    let mut tags = HashMap::new();
+    tags.insert(
+        "urn:x-nmos:tag:grouphint/v1.0".to_string(),
+        vec![format!("Media Function {}:Video", pid).to_string()],
+    );
+    let flow_def_details = FlowDefVideo {
+        grain_rate: Rate {
+            numerator: framerate.numer(),
+            denominator: framerate.denom(),
+        },
+        frame_width: width,
+        frame_height: height,
+        interlace_mode,
+        colorspace: colorimetry,
+        components: vec![
+            Component {
+                name: "Y".into(),
+                width,
+                height,
+                bit_depth: 10,
+            },
+            Component {
+                name: "Cb".into(),
+                width: width / 2,
+                height,
+                bit_depth: 10,
+            },
+            Component {
+                name: "Cr".into(),
+                width: width / 2,
+                height,
+                bit_depth: 10,
+            },
+        ],
+    };
+    let flow_def = FlowDef {
+        id: Uuid::parse_str(flow_id)
+            .map_err(|e| gst::loggable_error!(CAT, "Flow ID is invalid: {}", e))?,
+        description: format!(
+            "MXL Test Flow, {}p{}",
+            height,
+            framerate.numer() / framerate.denom()
+        ),
+        tags,
+        format: "urn:x-nmos:format:video".into(),
+        label: format!(
+            "MXL Test Flow, {}p{}",
+            height,
+            framerate.numer() / framerate.denom()
+        ),
+        parents: vec![],
+        media_type: format!("video/{}", format),
+        details: mxl::flowdef::FlowDefDetails::Video(flow_def_details),
+    };
+    let instance = &state.instance;
+
+    let (flow_writer, flow, is_created) = instance
+        .create_flow_writer(
+            serde_json::to_string(&flow_def)
+                .map_err(|e| gst::loggable_error!(CAT, "Failed to convert: {}", e))?
+                .as_str(),
+            None,
+        )
+        .map_err(|e| gst::loggable_error!(CAT, "Failed to create flow writer: {}", e))?;
+    if !is_created {
+        return Err(gst::loggable_error!(
+            CAT,
+            "The writer could not be created, the UUID belongs to a flow with another active writer"
+        ));
+    }
+    let writer = Some(
+        flow_writer
+            .to_grain_writer()
+            .map_err(|e| gst::loggable_error!(CAT, "Failed to create grain writer: {}", e))?,
+    );
+    let grain_rate = flow
+        .common()
+        .grain_rate()
+        .map_err(|e| gst::loggable_error!(CAT, "Failed to get grain rate: {}", e))?;
+    let index = instance.get_current_index(&grain_rate);
+    let instance = instance.clone();
+    let (tx, rx) = bounded::<VideoCommand>(MAX_CHANNEL_SIZE);
+    let sleep_flag_init = Arc::new((Mutex::new(false), Condvar::new()));
+    let sleep_flag = sleep_flag_init.clone();
+    thread::spawn(move || {
+        let mut engine = VideoEngine {
+            writer,
+            instance,
+            grain_rate,
+            sleep_flag,
+        };
+        await_video_buffer(&mut engine, rx)
+    });
+    let sleep_flag = sleep_flag_init.clone();
+    state.video = Some(VideoState {
+        grain_index: index,
+        initial_time: None,
+        latency: 0,
+        tx,
+        sleep_flag,
+    });
+    state.flow = Some(flow);
+
+    Ok(())
+}
